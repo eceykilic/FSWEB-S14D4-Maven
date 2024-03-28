16181a74913360a9ac35e69b4f36b5005ae4142b
eceykilic <eceykilic@gmail.com>
2024-03-28 12:28:19 +0300

    solutions

--- src/main/java/org/example/model/Store.java
+++ src/main/java/org/example/model/Store.java
@@ -1,12 +1,20 @@
 package org.example.model;
 
 public class Store {
-    public static void main(String[] args) {
-
-        //listProducts();
+    public static void listProducts(ProductForSale[] products) {
+        for (ProductForSale product : products) {
+            product.showDetails();
+            System.out.println();
+        }
     }
 
-    public static void listProducts(ProductForSale[] products) {
+    public static void main(String[] args) {
+        ProductForSale[] products = {
+                new Chocolate("Chocolate", 5, "Delicious chocolate bar"),
+                new Coke("Drink", 10, "Refreshing cola drink"),
+                new Bread("Food", 15, "Freshly baked bread")
+        };
 
+        listProducts(products);
     }
 }
